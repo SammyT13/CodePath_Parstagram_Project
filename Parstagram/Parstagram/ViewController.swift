07328df7f656605704435b0f3c465b8de6b5d25b
Sammy Torres <91271419+SammyT13@users.noreply.github.com>
2022-10-06 20:54:13 -0400

    Updated Login View Controller screen.  Added Navigation Controller.  Added Login and Feed View Controller files.

---
+++ Parstagram/Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Sammy Torres II on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
